018ebb78ca77b35fe87a55ecea1b5e17c8729675
Sofia Carballo <sofia.carballo@live.com>
2020-12-04 14:10:11 +0100

    Create Kata model, controller, migration, factory, seeder

---
+++ database/migrations/2020_12_04_122846_create_katas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKatasTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('katas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description', 200);
+            $table->text('level');
+            $table->string('source');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('katas');
+    }
+}
