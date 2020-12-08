4e08aa20e504c8c91af2427745d6ab0f9c075129
Sergi <solive@uic.es>
2020-12-08 19:15:23 +0100

    added PageController to handle static pages routing

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,12 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Page Controller
+Route::get('/', [App\Http\Controllers\PageController::class, 'home'])->name('home');
+Route::get('/group/randomizer', [App\Http\Controllers\PageController::class, 'randomizer'])->name('group-randomizer');
+Route::get('/kata/list', [App\Http\Controllers\PageController::class, 'kataList'])->name('kata-list');
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
