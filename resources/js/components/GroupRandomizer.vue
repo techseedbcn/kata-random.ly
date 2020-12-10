72beba2203614cac3ab914c5cb968c90fd7a0796
Sergi <solive@uic.es>
2020-12-10 23:27:37 +0100

    added last step on form

--- resources/js/components/GroupRandomizer.vue
+++ resources/js/components/GroupRandomizer.vue
@@ -7,27 +7,31 @@
                     <!-- NAV PILLS -->
                     <ul class="nav nav-pills">
                         <li class="nav-item">
-                            <a class="nav-link" :class="{'active': current_step == 1}" href="#" @click.prevent="goToStep(1)">Add Coders Taking Part</a>
+                            <a class="nav-link" :class="{'active': current_step == 1}" href="#" @click.prevent="goToStep(1)">Step 1: Adding Coders</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" :class="{'active': current_step == 2, 'disabled': max_step < 2}" href="#" @click.prevent="goToStep(2)">How many groups</a>
+                            <a class="nav-link" :class="{'active': current_step == 2, 'disabled': max_step < 2}" href="#" @click.prevent="goToStep(2)">Step 2: Form the Groups</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" :class="{'active': current_step == 3, 'disabled': max_step < 3}" href="#" @click.prevent="goToStep(3)">Get a Kata</a>
+                            <a class="nav-link" :class="{'active': current_step == 3, 'disabled': max_step < 3}" href="#" @click.prevent="goToStep(3)">Step 3: Get a Kata</a>
                         </li>
                     </ul>
                     <div class="card-body">
-                        <!-- TAB 1 -->
+                        <!-- TAB 1: ADDING PARTICIPANTS -->
                         <div class="tab-content border-0">
                             <div v-show="current_step==1">
+                                <p> 
+                                    <strong>Add all the coders taking part in the kata, and their preferred programming languages</strong>
+                                </p>
                                 <!-- DYNAMIC ADDING INPUTS (FORM CODERS)-->
-                                <div class="row g-2" v-for="(input, key) in inputs" :key="key">
+                                <div class="row" v-for="(input, key) in inputs" :key="key">
+                                
                                     <div class="col-md">
                                         <label for="floatingInputValue">Coder name</label>
                                         <input type="text" class="form-control" id="floatingInputValue" placeholder="Martin Fowler" v-model="input.name">
                                     </div>
                                     <div class="col-md">
-                                        <label for="floatingInputValue">Coder name</label>
+                                        <label for="floatingInputValue">Preferred Languages</label>
                                         <input type="text" class="form-control" id="floatingInputValue" placeholder="python,java,php,typescript" v-model="input.languages">
                                     </div>
                                     <div class="col-md">
@@ -38,7 +42,7 @@
                                                         <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
                                                 </svg>
                                             </a>
-                                            <a class="btn" click="removeCoder(key)" v-show="key || ( !key && inputs.length > 1)">
+                                            <a class="btn" @click="removeCoder(key)" v-show="key || ( !key && inputs.length > 1)">
                                                 <svg  width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-dash-circle-fill" fill="red" xmlns="http://www.w3.org/2000/svg">
                                                         <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7z"/>
                                                 </svg>
@@ -49,28 +53,34 @@
                             </div>
                         </div>
                         
-                        <!-- TAB 2 -->
+                        <!-- TAB 2: FORMING THE GROUPS -->
                         <div class="tab-content border-0">
                             <div v-show="current_step==2">
-                                <!-- FORM GROUPS -->
+                                <p> 
+                                    <strong>Select the number of groups you'd like to form OR the the size of the groups. We'll do the rest</strong>
+                                </p>
                                 <div class="row g-2">
                                     <div class="col-md">
                                         <label for="number-of-froups">Number of Groups</label>
                                         <input type="number" class="form-control" id="floatingInputValue" placeholder="3">
                                     </div>
-                                    
+                                    <div class="col-md">
+                                        <label for="group-size">Number of Groups</label>
+                                        <input type="number" class="form-control"  id="floatingInputValue" placeholder="2">
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
-                        <!-- TAB 3 -->
+                        <!-- TAB 3: GET A RANDOM KATA -->
                         <div class="tab-content border-0">
                             <div v-show="current_step==3">
-                                <!-- FORM GROUPS -->
+                                 <p> 
+                                    <strong>Do you already have a kata to work on?</strong>
+                                </p>
                                 <div class="row g-2" >
                                     <div class="col-md">
                                         <label for="level-select">Select the difficulty level of your kata</label>
-                                        
                                         <select id="level-select" class="form-select" aria-label="Default select example">
                                             <option value="1">Youngling</option>
                                             <option value="2">Padawan</option>
@@ -78,7 +88,18 @@
                                             <option value="3">Jedi Master</option>
                                         </select>
                                     </div>
-                                    
+                                    <div class="col-md">
+                                        <label for="topic-select">Do you want to target a specific topic/skill?</label>
+                                        <select id="topic-select" class="form-select" aria-label="Default select example">
+                                            <option value="1">Software Architecture</option>
+                                            <option value="2">Algorithms & Data Structures</option>
+                                            <option value="3">Object Oriented Design</option>
+                                            <option value="4">Design Patterns</option>
+                                            <option value="5">Refactoring</option>
+                                            <option value="6">Iterating Code</option>
+                                            <option value="7">Functional Programming</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -123,10 +144,15 @@
             nextStep() {
                 if(this.current_step == 3)
                 {
-                    return
+                    this.randomizeGroups();
+                    return;
                 }
                 this.current_step++;
                 this.max_step++;
+            },
+
+            randomizeGroups() {
+                alert('This should trigger')
             },
 
             goToStep(step) {
