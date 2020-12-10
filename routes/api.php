08d6a4dce6c95da9bf1853b07c8c69ffee5348e1
Sofia Carballo <sofia.carballo@live.com>
2020-12-10 17:56:12 +0100

    Add api routes to get all the katas, get one single kata and get one randomly

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KataController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/katas','KataController@showAll');
+Route::get('/katas', [KataController::class, 'getKatas']);
+Route::get('/katas/{kata}',[KataController::class, 'getKata']);
 
-Route::get('/katas/{kata}','KataController@getKata');
+Route::get('/random-kata', [KataController::class, 'getRandomKata']);
