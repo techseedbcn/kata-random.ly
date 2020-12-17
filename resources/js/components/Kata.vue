e4a16730ed596aac8e20b6bc91cd2cc8740d42f1
Sofia Carballo <sofia.carballo@live.com>
2020-12-17 11:16:14 +0100

    Delete commented section. Add route to Read more button. Modify class name for list

--- resources/js/components/Kata.vue
+++ resources/js/components/Kata.vue
@@ -1,43 +1,19 @@
 <template>
     <div class="container">
-        <!-- <div v-bind:key="i" v-for="(kata, i) in kataList">
-            <div class="card-alumni-s">
-                <div class="card-head-s">
-                    <h3>
-                        {{kata.name}}
-                    </h3>
-                    <div class="card-icons">
-                        <!-- <a :href="'/empleos/' + jobOffer.id"><i class="fas fa-info button-s"></i></a> -->
-                   <!--  </div>
-                </div>
-                <hr>
-                <div class="card-main-s">
-                    <p> {{(kata.description).slice(0, 150)}}... </p>
-                    <div class="card-dates-s">
-                    <p> <b>Level:  </b>{{kata.level}} </p>
-                    <p> <b>Source:  </b>{{kata.source}} </p>
-                    </div>
-                </div>
-                <div class="card-foot-s">
-                    <div class="tag-list-job-offer">
-                        <p> hola </p>
-                    </div>
-                </div>
-            </div>
-        </div> -->
-            <div class="course-list">
-                <div class="card kata-card" style="width: 18rem;" v-bind:key="i" v-for="(kata, i) in kataList">
-                    <img class="card-img-top" src="./background-light.jpg" alt="Card image cap">
-                    <div class="card-body">
+        <h1 class="mb-4"> Katas </h1>
+        <div class="kata-list">
+            <div class="card kata-card" style="width: 18rem;" v-bind:key="i" v-for="(kata, i) in kataList">
+                <img class="card-img-top" src="/images/background-light.jpg" alt="Card image cap">
+                <div class="card-body">
                     <h5 class="card-title">{{kata.name}}</h5>
                     <p class="card-text">{{(kata.description).slice(0, 150)}}...</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                    <div class="card-footer">
+                    <a :href="'/katas/' + kata.id" class="btn btn-primary float-right">Read more</a>
+                </div>
+                <div class="card-footer">
                     <small class="text-muted">{{kata.level}}</small>
-                    </div>
                 </div>
             </div>
+        </div>
     </div>
 </template>
 
@@ -64,9 +40,8 @@
 </script>
 
 <style scoped>
-    .course-list {
+    .kata-list {
     max-height: 70vh;
-    /* overflow-y: scroll; */
     display: grid;
     grid-template-columns: repeat(auto-fill, 350px);
     grid-gap: 20px;
